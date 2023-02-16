03ad219125561a21d94908ae78dba337f0615788
SyedShozabZaidi <shozthegreat@gmail.com>
2023-02-16 17:50:14 +0500

    first commit

---
+++ grade.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter your marks"))
+if marks>=90:
+    print("Congratualations for A1 Grade")
+elif marks>=80 and marks<=90:
+    print("Congratulations for A Grade")
+elif marks>=70 and marks<=80:
+    print("Congratulations for B Grade")
+elif marks>=60 and marks<=70:
+    print("Congratulations for c Grade")
+elif marks>=50 and marks<=60:
+    print("Congratulations for D Grade")
+elif marks>=0 and marks<=10:
+    print("Failed")
+
+
